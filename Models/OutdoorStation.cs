41041e3dd1f1bd9ce565cfedd670dc30bfa8fc19
ravivostok-beep <ravi.vostok@gmail.com>
2026-09-09 17:42:19 +0400

    Change IpAddress and Password in OutdoorStation
    Updated the IpAddress and Password properties for security and configuration changes.

--- Models/OutdoorStation.cs
+++ Models/OutdoorStation.cs
@@ -6,11 +6,11 @@ public class OutdoorStation
 {
     public string Name { get; set; } = "MAIN OUTDOOR";
     public string Model { get; set; } = "DS-K1T502DBFWX-C";
-    public string IpAddress { get; set; } = "192.168.0.65";
+    public string IpAddress { get; set; } = "192.168.5.133";
     public int HttpPort { get; set; } = 80;
     public int RtspPort { get; set; } = 554;
     public string Username { get; set; } = "admin";
-    public string Password { get; set; } = "";
+    public string Password { get; set; } = "Vos@3558817";
     public string MainIndoorName { get; set; } = "MAIN INDOOR";
     public string RoomNumber { get; set; } = "101";
     public string ExtensionName { get; set; } = "INDOOR EXTENSION 01";

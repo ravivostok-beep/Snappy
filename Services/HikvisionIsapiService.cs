f5b864a789280d2f45a308f54a71e114dbee584a
ravivostok-beep <ravi.vostok@gmail.com>
2026-09-09 16:45:08 +0400

    Refactor HikvisionIsapiService constructor and methods

--- Services/HikvisionIsapiService.cs
+++ Services/HikvisionIsapiService.cs
@@ -10,108 +10,119 @@ namespace SNAPPY.Services;
 
 public sealed class HikvisionIsapiService : IDisposable
 {
-private readonly HttpClient _http;
+    private readonly HttpClient _http;
 
-```
-public HikvisionIsapiService(
-    string ip,
-    int port,
-    string username,
-    string password)
-{
-    var handler = new HttpClientHandler
+    public HikvisionIsapiService(
+        string ip,
+        int port,
+        string username,
+        string password)
     {
-        Credentials = new NetworkCredential(username, password),
-        PreAuthenticate = false
-    };
-
-    _http = new HttpClient(handler)
-    {
-        BaseAddress = new Uri($"http://{ip}:{port}"),
-        Timeout = TimeSpan.FromSeconds(5)
-    };
-}
-
-public async Task<string> GetCallStatusAsync(
-    CancellationToken token = default)
-{
-    using var response = await _http.GetAsync(
-        "/ISAPI/VideoIntercom/callStatus?format=json",
-        token);
-
-    return await response.Content.ReadAsStringAsync(token);
-}
-
-public Task<string> AnswerAsync(
-    CancellationToken token = default)
-{
-    return SendCallSignalAsync("answer", token);
-}
-
-public Task<string> RejectAsync(
-    CancellationToken token = default)
-{
-    return SendCallSignalAsync("reject", token);
-}
-
-public Task<string> HangUpAsync(
-    CancellationToken token = default)
-{
-    return SendCallSignalAsync("hangUp", token);
-}
-
-private async Task<string> SendCallSignalAsync(
-    string command,
-    CancellationToken token)
-{
-    var json = JsonSerializer.Serialize(new
-    {
-        CallSignal = new
+        var handler = new HttpClientHandler
         {
-            cmdType = command
-        }
-    });
+            Credentials = new NetworkCredential(
+                username,
+                password),
 
-    using var content = new StringContent(
-        json,
-        Encoding.UTF8,
-        "application/json");
+            PreAuthenticate = false,
 
-    using var response = await _http.PutAsync(
-        "/ISAPI/VideoIntercom/callSignal?format=json",
-        content,
-        token);
+            UseCookies = false
+        };
 
-    return await response.Content.ReadAsStringAsync(token);
-}
+        _http = new HttpClient(handler)
+        {
+            BaseAddress = new Uri(
+                $"http://{ip}:{port}"),
 
-public async Task<string> UnlockDoorAsync(
-    CancellationToken token = default)
-{
-    const string xml =
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-        "<RemoteControlDoor version=\"2.0\" " +
-        "xmlns=\"http://www.isapi.org/ver20/XMLSchema\">" +
-        "<cmd>open</cmd>" +
-        "</RemoteControlDoor>";
+            Timeout = TimeSpan.FromSeconds(5)
+        };
+    }
 
-    using var content = new StringContent(
-        xml,
-        Encoding.UTF8,
-        "application/xml");
+    public async Task<string> GetCallStatusAsync(
+        CancellationToken token = default)
+    {
+        using var response = await _http.GetAsync(
+            "/ISAPI/VideoIntercom/callStatus?format=json",
+            token);
 
-    using var response = await _http.PutAsync(
-        "/ISAPI/AccessControl/RemoteControl/door/1",
-        content,
-        token);
+        return await response.Content.ReadAsStringAsync(token);
+    }
 
-    return await response.Content.ReadAsStringAsync(token);
-}
+    public Task<string> AnswerAsync(
+        CancellationToken token = default)
+    {
+        return SendCallSignalAsync(
+            "answer",
+            token);
+    }
 
-public void Dispose()
-{
-    _http.Dispose();
-}
-```
+    public Task<string> RejectAsync(
+        CancellationToken token = default)
+    {
+        return SendCallSignalAsync(
+            "reject",
+            token);
+    }
 
+    public Task<string> HangUpAsync(
+        CancellationToken token = default)
+    {
+        return SendCallSignalAsync(
+            "hangUp",
+            token);
+    }
+
+    private async Task<string> SendCallSignalAsync(
+        string command,
+        CancellationToken token)
+    {
+        var json = JsonSerializer.Serialize(
+            new
+            {
+                CallSignal = new
+                {
+                    cmdType = command
+                }
+            });
+
+        using var content = new StringContent(
+            json,
+            Encoding.UTF8,
+            "application/json");
+
+        using var response = await _http.PutAsync(
+            "/ISAPI/VideoIntercom/callSignal?format=json",
+            content,
+            token);
+
+        return await response.Content.ReadAsStringAsync(token);
+    }
+
+    public async Task<string> UnlockDoorAsync(
+        CancellationToken token = default)
+    {
+        const string xml =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+            "<RemoteControlDoor version=\"2.0\" " +
+            "xmlns=\"http://www.isapi.org/ver20/XMLSchema\">" +
+            "<cmd>open</cmd>" +
+            "</RemoteControlDoor>";
+
+        using var content = new StringContent(
+            xml,
+            Encoding.UTF8,
+            "application/xml");
+
+        using var response = await _http.PutAsync(
+            "/ISAPI/AccessControl/RemoteControl/door/1",
+            content,
+            token);
+
+        return await response.Content.ReadAsStringAsync(token);
+    }
+
+    public void Dispose()
+    {
+        _http.Dispose();
+    }
 }

88e7d24e083af7bf8037453ae4410a2023269f58
ravivostok-beep <ravi.vostok@gmail.com>
2026-09-10 09:39:55 +0400

    Add files via upload

--- Models/OutdoorStation.cs
+++ Models/OutdoorStation.cs
@@ -2,21 +2,30 @@ using System;
 
 namespace SNAPPY.Models;
 
-public class OutdoorStation
+public sealed class OutdoorStation
 {
-    public string Name { get; set; } = "MAIN OUTDOOR";
-    public string Model { get; set; } = "DS-K1T502DBFWX-C";
+    public int Id { get; set; }
+    public string Name { get; set; } = "OUTDOOR 01";
     public string IpAddress { get; set; } = "192.168.0.65";
     public int HttpPort { get; set; } = 80;
     public int RtspPort { get; set; } = 554;
     public string Username { get; set; } = "admin";
-    public string Password { get; set; } = "";
+    public string Password { get; set; } = string.Empty;
+    public string DeviceModel { get; set; } = "DS-K1T502DBFWX-C";
     public string MainIndoorName { get; set; } = "MAIN INDOOR";
     public string RoomNumber { get; set; } = "101";
     public string ExtensionName { get; set; } = "INDOOR EXTENSION 01";
     public string ExtensionNumber { get; set; } = "1";
 
-    public string RtspUrl =>
-        $"rtsp://{Uri.EscapeDataString(Username)}:{Uri.EscapeDataString(Password)}@" +
-        $"{IpAddress}:{RtspPort}/Streaming/Channels/101";
+    public string RtspUrl
+    {
+        get
+        {
+            var user = Uri.EscapeDataString(Username ?? string.Empty);
+            var pass = Uri.EscapeDataString(Password ?? string.Empty);
+            return $"rtsp://{user}:{pass}@{IpAddress}:{RtspPort}/Streaming/Channels/101";
+        }
+    }
+
+    public override string ToString() => $"{Name}  •  {IpAddress}";
 }
